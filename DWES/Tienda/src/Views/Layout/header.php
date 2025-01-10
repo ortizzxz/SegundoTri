a79fa79944cd5f8ed57fa5dd650fb7151a8873b9
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-01-10 14:20:31 +0100

    update

--- DWES/Tienda/src/Views/Layout/header.php
+++ DWES/Tienda/src/Views/Layout/header.php
@@ -2,7 +2,12 @@
   <ul>
     <li><a class="active" href="<?php echo BASE_URL; ?>">Tienda Online</a></li>
     <?php if(isset($_SESSION['identity'])):?>
-      <li><p>Hola <?= $_SESSION['identity']['nombre']; ?> </p></li>
+      <p>Hola <?= $_SESSION['identity']['nombre']; ?> </p>
+      <?php if($_SESSION['identity']['rol'] === 'admin'):?>
+        <li><a href="<?=  BASE_URL; ?>">Gestionar pedidos</a></li>
+        <li><a href="<?=  BASE_URL; ?>">Gestionar productos</a></li>
+        <li><a href="<?=  BASE_URL; ?>">Gestionar categorias</a></li>
+      <?php endif; ?> 
       <li><a href="<?=  BASE_URL; ?>">Carrito</a></li>
       <li><a href="<?=  BASE_URL; ?>">Cerrar Sesión</a></li>
     <?php else: ?> 
