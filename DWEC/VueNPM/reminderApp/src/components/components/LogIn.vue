94c7f7fcbcd6324f91b660b8ca72f7c416376c94
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-01-15 12:38:59 +0100

    auth

---
+++ DWEC/VueNPM/reminderApp/src/components/components/LogIn.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>login component</h1>
+</template>
+
+<style scoped>
+</style>
--- DWEC/VueNPM/reminderApp/src/components/components/NoteContainerHeader.vue
+++ DWEC/VueNPM/reminderApp/src/components/components/NoteContainerHeader.vue
@@ -1,11 +1,48 @@
 <script setup>
+import { GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, signOut } from "firebase/auth";
+import { useCurrentUser, useFirebaseAuth } from "vuefire";
 
+const user = useCurrentUser();
+const auth = useFirebaseAuth();
+const googleAuthProvider = new GoogleAuthProvider();
+
+function logIn() {
+  signInWithPopup(auth, googleAuthProvider)
+    .then(() => console.log("validacion correcta"))
+    .catch((reason) => {
+      console.error("Failed validation", reason);
+    });
+}
+function logOut() {
+  signOut(auth)
+    .then(() => console.log("signout correcto"))
+    .catch((reason) => {
+      console.error("Failed signout", reason);
+    });
+}
 </script>
-
 <template>
-    <h1>Proyecto Vue.js - Jesús Ortiz</h1>
-</template>
-
-<style scoped>
-
-</style>
+    <h1 v-if="user">
+      Hola {{ user.displayName }} -
+      <span @click="logOut" class="logout-span">Log Out</span>
+    </h1>
+  </template>
+  
+  <style scoped>
+  .logout-span {
+    border: 1px solid rgb(75, 28, 28);
+    display: inline-block;
+    padding: 5px 10px;
+    color: white;
+    border-radius: 3px;
+    cursor: pointer;
+    font-size: 1.5rem;
+    transition: background-color 0.3s ease;
+  }
+  
+  .logout-span:hover {
+    background-color: #d32f2f;
+  }
+  </style>
+  
