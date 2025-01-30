85a6b7bc31ec7f02f0b9a1e5b39dd7b20096b4c9
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-01-30 09:09:52 +0100

    upload

--- DWEC/Pokeapi/pokeapi/src/firebase.jsx
+++ DWEC/Pokeapi/pokeapi/src/firebase.jsx
@@ -2,7 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAUPlJ0jSwSU1gXe9ljyMZ4kiZHyYMUTvo",
+  authDomain: "pokeapp-3f1a6.firebaseapp.com",
+  projectId: "pokeapp-3f1a6",
+  storageBucket: "pokeapp-3f1a6.firebasestorage.app",
+  messagingSenderId: "878414489246",
+  appId: "1:878414489246:web:9bbbad921ac179eb9e3d8f"
+};
 
 const app = initializeApp(firebaseConfig);
 
