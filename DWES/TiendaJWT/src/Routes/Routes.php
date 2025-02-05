e4b1abbca31d6f587b9e0252db7c402f2210ff5b
Jesus Ortiz <jesusdortiz18@gmail.com>
2025-02-05 12:53:38 +0100

    all

--- DWES/TiendaJWT/src/Routes/Routes.php
+++ DWES/TiendaJWT/src/Routes/Routes.php
@@ -146,7 +146,6 @@ class Routes
             }
         });
         
-<<<<<<< HEAD
         Router::add('GET', '/forgot-password', function () {
             (new AuthController())->forgotPassword();
         });
@@ -171,8 +170,6 @@ class Routes
             (new CartController())->addProduct($id);
         });
         
-=======
->>>>>>> 381a7006baff64be86b8025f8a82287b465a96b8
         
 
         Router::dispatch();
